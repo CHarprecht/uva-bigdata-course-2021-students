72b22e8831f0b66e95f682d357eb05cdd8c40ef8
Carlo Harprecht <43964402+CHarprecht@users.noreply.github.com>
2021-02-12 19:47:09 +0100

    Update FilteringWordCount.java

--- src/main/java/nl/uva/bigdata/hadoop/assignment1/FilteringWordCount.java
+++ src/main/java/nl/uva/bigdata/hadoop/assignment1/FilteringWordCount.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class FilteringWordCount extends HadoopJob {
 
@@ -32,17 +33,38 @@ public class FilteringWordCount extends HadoopJob {
     }
 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable ONE = new IntWritable(1);
+        private final Text word = new Text();
 
         public void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-            // TODO Implement me
+            StringTokenizer tokenizer = new StringTokenizer(value.toString().replaceAll("[^a-zA-Z ]", ""));
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken().toLowerCase());
+                if (!(word.toString().equals("to")) && !(word.toString().equals("the"))) {
+                    context.write(word, ONE);
+                    System.out.println("Mapped: "+word +" with count: "+ONE);
+                }
+                else {
+                    System.out.println("Filtered: "+word);
+                }
+
+            }
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
-                throws IOException, InterruptedException {
-            // TODO Implement me
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private final IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+
+            System.out.println("Reduced: "+key+" with result: "+result);
         }
     }
 }
