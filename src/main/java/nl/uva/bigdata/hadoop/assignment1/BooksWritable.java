6781009a2255f1eedf62b1a6aacaf7fd70a7cb78
Carlo Harprecht <43964402+CHarprecht@users.noreply.github.com>
2021-02-12 11:53:30 +0100

    ch

--- src/main/java/nl/uva/bigdata/hadoop/assignment1/BooksWritable.java
+++ src/main/java/nl/uva/bigdata/hadoop/assignment1/BooksWritable.java
@@ -2,6 +2,7 @@ package nl.uva.bigdata.hadoop.assignment1;
 
 import org.apache.hadoop.io.Writable;
 
+import javax.print.DocFlavor;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
@@ -17,12 +18,26 @@ public class BooksWritable  implements Writable {
 
     @Override
     public void write(DataOutput out) throws IOException {
-        // TODO Implement me
-    }
+        out.writeInt(this.books.length);
 
+        for (int i = 0; i < this.books.length; i = i + 1) {
+            out.writeUTF(this.books[i].getTitle());
+            out.writeInt(this.books[i].getYear());
+        }
+        System.out.println("Wrote all "+this.books.length+ " books");
+    }
     @Override
     public void readFields(DataInput in) throws IOException {
-        // TODO Implement me
+        int length = in.readInt();
+
+        Book[] books = new Book[length];
+        for (int j = 0; j < length; j = j + 1) {
+            String title = in.readUTF();
+            int year = in.readInt();
+            books[j] = new Book(title, year);
+        }
+        setBooks(books);
+        System.out.println("Read all "+length+" books");
     }
 
     @Override
